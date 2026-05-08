e7bb5209121f5e035b0de3a6b357183e650ac305
ProgrammerA <aireater514@gmail.com>
2026-05-08 22:11:31 +0900

    微修正

--- update.cpp
+++ update.cpp
@@ -5,17 +5,20 @@ int main()
 {
     try
     {
+        std::filesystem::path template_cpp = "template.cpp";
+        std::filesystem::path ABC = "ABC";
 
-        if (!std::filesystem::copy_file("template.cpp", "ABC/A.cpp", std::filesystem::copy_options::overwrite_existing) ||
-            !std::filesystem::copy_file("template.cpp", "ABC/B.cpp", std::filesystem::copy_options::overwrite_existing) ||
-            !std::filesystem::copy_file("template.cpp", "ABC/C.cpp", std::filesystem::copy_options::overwrite_existing) ||
-            !std::filesystem::copy_file("template.cpp", "ABC/D.cpp", std::filesystem::copy_options::overwrite_existing) ||
-            !std::filesystem::copy_file("template.cpp", "ABC/E.cpp", std::filesystem::copy_options::overwrite_existing) ||
-            !std::filesystem::copy_file("template.cpp", "ABC/F.cpp", std::filesystem::copy_options::overwrite_existing) ||
-            !std::filesystem::copy_file("template.cpp", "ABC/G.cpp", std::filesystem::copy_options::overwrite_existing) ||
-            !std::filesystem::copy_file("template.cpp", "ABC/H.cpp", std::filesystem::copy_options::overwrite_existing) ||
-            !std::filesystem::copy_file("template.cpp", "ABC/I.cpp", std::filesystem::copy_options::overwrite_existing) ||
-            !std::filesystem::copy_file("template.cpp", "code.cpp", std::filesystem::copy_options::overwrite_existing))
+        if (!std::filesystem::copy_file(template_cpp, ABC / "A.cpp", std::filesystem::copy_options::overwrite_existing) ||
+            !std::filesystem::copy_file(template_cpp, ABC / "B.cpp", std::filesystem::copy_options::overwrite_existing) ||
+            !std::filesystem::copy_file(template_cpp, ABC / "C.cpp", std::filesystem::copy_options::overwrite_existing) ||
+            !std::filesystem::copy_file(template_cpp, ABC / "D.cpp", std::filesystem::copy_options::overwrite_existing) ||
+            !std::filesystem::copy_file(template_cpp, ABC / "E.cpp", std::filesystem::copy_options::overwrite_existing) ||
+            !std::filesystem::copy_file(template_cpp, ABC / "F.cpp", std::filesystem::copy_options::overwrite_existing) ||
+            !std::filesystem::copy_file(template_cpp, ABC / "G.cpp", std::filesystem::copy_options::overwrite_existing) ||
+            !std::filesystem::copy_file(template_cpp, ABC / "H.cpp", std::filesystem::copy_options::overwrite_existing) ||
+            !std::filesystem::copy_file(template_cpp, ABC / "I.cpp", std::filesystem::copy_options::overwrite_existing) ||
+            
+            !std::filesystem::copy_file(template_cpp, "code.cpp", std::filesystem::copy_options::overwrite_existing))
         {
             throw std::runtime_error("Failed to copy file");
         }
